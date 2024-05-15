361bc32d141ad0f766b23e6d6c54c4b00c54cbeb
Icarus0989 <155402303+Icarus0989@users.noreply.github.com>
2024-05-14 23:32:17 -0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <StudyDetail studyId="1" />
+  </div>
+</template>
+
+<script>
+import StudyDetail from './components/StudyDetail.vue';
+
+export default {
+  name: 'App',
+  components: {
+    StudyDetail
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
